c8f60b0871cc2082d33ec9f6ad5168195b1347d9
Desmond Onam <desmondonam@gmail.com>
2022-09-06 00:22:41 +0300

    add comment

--- quadratic.py
+++ quadratic.py
@@ -18,4 +18,6 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
+# Print out the solutions to the problem
+
 print('The solution are {0} and {1}'.format(sol1,sol2))
